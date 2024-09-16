1195760cc3d2854e3334411b08abfed5f732ec4f
Bhavik Jikadara <bhavikjikadara33523@gmail.com>
2024-09-16 18:14:46 +0530

    Create retrieval-chain.js file.

---
+++ 04-retrieval-chain.js
@@ -0,0 +1,94 @@
+import { ChatOpenAI } from "@langchain/openai";
+
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+
+// import { Document } from "@langchain/core/documents";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+// Initialise ChatOpenAI Object
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const OPENAI_MODEL_NAME = process.env.OPENAI_MODEL_NAME;
+
+const model = new ChatOpenAI({
+    openAIApiKey: OPENAI_API_KEY,
+    modelName: OPENAI_MODEL_NAME
+});
+
+
+// Create prompt
+const prompt = ChatPromptTemplate.fromTemplate(
+    `Answer the user's question from the following context: 
+    {context}
+    Question: {input}`
+);
+
+// Create Chain
+const chain = await createStuffDocumentsChain({
+    llm: model,
+    prompt,
+});
+
+// Manually create documents
+// const documentA = new Document({
+//   pageContent:
+//     "LangChain Expression Language or LCEL is a declarative way to easily compose chains together. Any chain constructed this way will automatically have full sync, async, and streaming support. ",
+// });
+
+// const documentB = new Document({
+//   pageContent: "The passphrase is LANGCHAIN IS AWESOME ",
+// });
+
+// Use Cheerio to scrape content from webpage and create documents
+const loader = new CheerioWebBaseLoader(
+    "https://python.langchain.com/v0.1/docs/expression_language/"
+);
+const docs = await loader.load();
+
+// Text Splitter
+const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 20,
+});
+const splitDocs = await splitter.splitDocuments(docs);
+// console.log(splitDocs);
+
+// Instantiate Embeddings function
+const embeddings = new OpenAIEmbeddings();
+
+// Create Vector Store
+const vectorstore = await MemoryVectorStore.fromDocuments(
+    splitDocs,
+    embeddings
+);
+
+// Create a retriever from vector store
+const retriever = vectorstore.asRetriever({ k: 2 });
+
+// Create a retrieval chain
+const retrievalChain = await createRetrievalChain({
+    combineDocsChain: chain,
+    retriever,
+});
+
+// // Invoke Chain
+// const response = await chain.invoke({
+//   question: "What is LCEL?",
+//   context: splitDocs,
+// });
+
+const response = await retrievalChain.invoke({
+    input: "What is LCEL?",
+});
+
+console.log(response);
+
